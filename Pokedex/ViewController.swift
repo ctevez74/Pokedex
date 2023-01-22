50b5f573cc483df13bbb5188cac91622fabb22d1
Carlos Tevez <cgtevez@icloud.com>
2023-01-21 23:04:36 -0300

    initial commit

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Carlos Gabriel Tevez on 21/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,12 @@
+# Pokedex Challenge
+Pokedex Challenge is a example Pokedex app written in Swift that implements the [PokeAPI](https://pokeapi.co).  
+
+This sample app demonstrates:
+
+# Architecture 🏛
+
+# Dependencies 
+
+# Todo 📝
+
+# Requirements ❗️
